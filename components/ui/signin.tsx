b6c44c0d9c85060263af51f738b213714a0c47db
Noe Villamor <noejayar@gmail.com>
2025-10-04 22:20:34 +0800

    Added prisma adapter

--- components/ui/signin.tsx
+++ components/ui/signin.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Signin = () => {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div>
       <button onClick={() => signIn("google")}>Signin</button>
+      <button onClick={() => signOut()}>Signin</button>
     </div>
   );
 };
